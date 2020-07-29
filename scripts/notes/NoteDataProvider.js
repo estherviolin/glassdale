bb7c5cf584eb2d01405e77bfe4096dc73e250971
estherviolin <esthernaomi.violin@gmail.com>
2020-07-29 14:57:05 -0500

    added notes form and save show and hide buttons

---
+++ scripts/notes/NoteDataProvider.js
@@ -0,0 +1,41 @@
+//array to be filled w notes
+let notes = []
+
+const eventHub = document.querySelector(".container")
+
+//function to dispatch change event that app state changed
+const dispatchStateChangeEvent = () => {
+    const noteStateChangedEvent = new CustomEvent("noteStateChanged")
+    eventHub.dispatchEvent(noteStateChangedEvent)
+}
+
+//function to make a copy of notes array
+export const useNotes = () => {
+    return notes.slice()
+}
+
+//function to get notes from API
+export const getNotes = () => {
+    return fetch("http://localhost:8088/notes")
+        //turn it into JSON
+        .then(response => response.json())
+        .then(parsedNotes => {
+            notes = parsedNotes
+        })
+}
+
+//function to save note after entered into form
+export const saveNote = (noteObj) => {
+    const jsonNote = JSON.stringify(noteObj) //turn object into string/strings
+
+    return fetch("http://localhost:8088/notes", {
+        method: "POST", //send it back to API?
+        //what is this object?
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: jsonNote //body is the stringified object
+    })
+    .then(getNotes) //get updated data from the API
+    .then(dispatchStateChangeEvent) //event that says app state was changed
+}
---
+++ scripts/notes/NoteForm.js
@@ -0,0 +1,42 @@
+import { saveNote } from "./NoteDataProvider.js"
+
+const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector(".noteFormContainer")
+
+// Handle browser-generated click event in component
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveNote") {
+
+        const noteTitle = document.querySelector("#note--title")
+        const noteAuthor = document.querySelector("#note--author")
+        const noteContent = document.querySelector("#note--content")
+        const noteSuspect = document.querySelector("#note--suspect")
+
+        // Make a new object representation of a note
+        const newNote = {
+            title: noteTitle.value,
+            author: noteAuthor.value,
+            suspect: noteSuspect.value,
+            content: noteContent.value,
+            timestamp: Date.now()
+        }
+
+        // Change API state and application state
+        saveNote(newNote)
+    }
+})
+
+const render = () => {
+    contentTarget.innerHTML = `
+        <h2 id="note--header">Notes:</h2>
+        <input type="text" id="note--title" placeholder="Enter note title" />
+        <input type="text" id="note--author" placeholder="Your name here" />
+        <input type="test" id="note--suspect" placeholder"Enter suspect involved" />
+        <textarea id="note--content" placeholder="Note text here"></textarea>
+        <button id="saveNote">Save Note</button>
+    `
+}
+
+export const NoteForm = () => {
+    render()
+}
