e3fb4f15a28be9b5cee7df6c8f9cbfd80b02fdff
estherviolin <esthernaomi.violin@gmail.com>
2020-08-04 10:41:51 -0500

    added hide witness button

--- scripts/witnesses/WitnessStatements.js
+++ scripts/witnesses/WitnessStatements.js
@@ -1,10 +1,12 @@
 //button that lists witness statements
 
 import { WitnessStatementList } from "./WitnessList.js"
+import { CriminalList } from "../criminals/CriminalList.js"
 
 
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".witnessButtonContainer")
+const hideButtonTarget = document.querySelector(".hideWitnessButton")
 
 //Witness button that will render on initial page load
 export const WitnessButton = () => {
@@ -14,11 +16,25 @@ export const WitnessButton = () => {
 
 }
 
+export const HideWitnessButton = () => {
+    hideButtonTarget.innerHTML = `
+    <button id="hideWitnesses">Hide</button>
+    `
+
+}
+
 //browser generated change event
 export const WitnessStatementsEvent = () => {
     eventHub.addEventListener("click", (clickEvent) => {
     if (clickEvent.target.id === "witnessStatements") {  
+        contentTarget.innerHTML = ``
         WitnessStatementList()
+        HideWitnessButton()
+    }
+    else if (clickEvent.target.id === "hideWitnesses") {
+        hideButtonTarget.innerHTML = ``
+        WitnessButton()
+        CriminalList()
     }
 })
 }
